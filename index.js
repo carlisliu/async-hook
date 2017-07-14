9e50bf74ba284183b0980a0bc3b9bf34ee9c3ab8
Carlis Liu <carlisliu@live.com>
2017-07-14 15:02:33 +0800

    fix variable name

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const asyncHooks = require('async_hooks');
 const fs = require('fs');
 
-async_hooks.createHook({
+asyncHooks.createHook({
     init(asyncId, type, triggerId) {
-        const cId = async_hooks.currentId();
+        const cId = asyncHooks.currentId();
         fs.writeSync(1, `${type}(${asyncId}): trigger: ${triggerId} scope: ${cId}\n`);
     }
 }).enable();
