1fc10f25bab3124f310865a51636e0bcda15b7be
Carlis Liu <carlisliu@live.com>
2017-07-14 15:01:20 +0800

    create aysnc hook

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 const asyncHooks = require('async_hooks');
+const fs = require('fs');
 
-console.log(asyncHooks);
+async_hooks.createHook({
+    init(asyncId, type, triggerId) {
+        const cId = async_hooks.currentId();
+        fs.writeSync(1, `${type}(${asyncId}): trigger: ${triggerId} scope: ${cId}\n`);
+    }
+}).enable();
+
+require('net').createServer((conn) => {}).listen(3000);
